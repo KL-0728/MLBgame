5a35792703e4aea218c9478ad5443fa4dc90dffe
KL-0728 <likaili0728@gmail.com>
2026-06-08 17:23:40 +0800

    Initial Commit

---
+++ MLB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MLB
+//
+//  Created by 11344113 on 6/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MLB/MLBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLBApp.swift
+//  MLB
+//
+//  Created by 11344113 on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct MLBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
